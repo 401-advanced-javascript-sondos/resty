674ecd5c187b3452c261c74def2d48238072d847
SondosMatahen <sondosmatahen@gmail.com>
2020-10-12 17:31:45 +0300

    Updates

---
+++ precache-manifest.72bb782877ef1a9faa4fbc8273111c30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35d79e8fcbc6fc8d79acee10fffa6288",
+    "url": "/resty/index.html"
+  },
+  {
+    "revision": "c59d72dc68ef1c7342c8",
+    "url": "/resty/static/css/main.75c95fea.chunk.css"
+  },
+  {
+    "revision": "06f2f7bc2e88d567ae85",
+    "url": "/resty/static/js/2.4d5f80d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resty/static/js/2.4d5f80d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c59d72dc68ef1c7342c8",
+    "url": "/resty/static/js/main.395d03d5.chunk.js"
+  },
+  {
+    "revision": "6972438e44fdcad64bce",
+    "url": "/resty/static/js/runtime-main.b658189f.js"
+  }
+]);
